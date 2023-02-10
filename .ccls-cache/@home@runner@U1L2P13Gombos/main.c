d7320068fc02ebaa8e71363c6f4c57a2d0e31329
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-10 13:12:05 +0000

    Iteration 1

--- .ccls-cache/@home@runner@U1L2P13Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P13Gombos/main.c
@@ -3,9 +3,17 @@
   Weekly Salary
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
+int main(void) { //define main function
+  float salary, weekly; //intialize variable as float
+
+  printf("Yearly Salary: "); //ask for yearly salary
+  scanf("%f", &salary); //accept input and format as float
+
+  weekly = salary/52; //find weekly salary
+
+  printf("Weekly Salary: $%0.2f", weekly); //print weekly salary
   
   return 0;
 }
