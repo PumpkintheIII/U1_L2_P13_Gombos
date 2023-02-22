f28c2d49832829d622689b8d1f5c07c0e6cb8350
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-22 19:41:26 +0000

    Iteration 2

--- .ccls-cache/@home@runner@U1L2P13Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P13Gombos/main.c
@@ -8,6 +8,8 @@
 int main(void) { //define main function
   float salary, weekly; //intialize variable as float
 
+  printf("This program allows you to enter your yearly salary and the program will output your weekly salary. You will get a prompt to enter your yearly salary:\n\t- Yearly Salary: Please enter your yearly salary here\n"); //instructions
+  
   printf("Yearly Salary: "); //ask for yearly salary
   scanf("%f", &salary); //accept input and format as float
 
